0a20af828156cc5f885489c1cd2fedea37ae27a1
Weijie <raoh.wu@gmail.com>
2021-09-03 11:21:37 +0800

    - update run.sh & debug.sh

--- debug.sh
+++ debug.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-pip3 install --no-cache-dir -r ./requirements.txt
-
 export REMOTE_DEBUG=1
 python productpage.py 9080
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip3 install --no-cache-dir -r ./requirements.txt
 python productpage.py 9080
 
